7a31bdce479578d469d20257cb940239ef057163
GoForBroke <go.for.broke1006@gmail.com>
2019-03-25 03:28:46 +0300

    ugly working version (refactoring required)

--- cmd/sport-archive-svc/main.go
+++ cmd/sport-archive-svc/main.go
@@ -1,31 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/google/logger"
+	"github.com/gorilla/rpc"
+	"github.com/gorilla/rpc/json"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	_ "github.com/mattn/go-sqlite3"
-	zipkinhttp "github.com/openzipkin/zipkin-go/middleware/http"
+	"github.com/streadway/amqp"
 
 	"github.com/goforbroke1006/sport-archive-svc/pkg/domain"
 	"github.com/goforbroke1006/sport-archive-svc/pkg/endpoint"
-	"github.com/goforbroke1006/sport-archive-svc/pkg/handler"
-	"github.com/goforbroke1006/sport-archive-svc/pkg/http"
 	"github.com/goforbroke1006/sport-archive-svc/pkg/service"
 	"github.com/goforbroke1006/sport-archive-svc/pkg/trace"
 )
 
 var (
-	dbConnStr     = flag.String("db-conn", "./sport-archive.db", "")
-	serveAddr     = flag.String("serve-addr", "0.0.0.0:8080", "")
-	allowSaveData = flag.Bool("allow-save", true, "")
-	logPath       = flag.String("log-path", "./access.log", "")
-	verbose       = flag.Bool("verbose", true, "Print info level logs to stdout")
+	dbConnStr   = flag.String("db-conn", "./sport-archive.db", "")
+	amqpConnStr = flag.String("amqp-conn", "amqp://guest:guest@localhost:5672/", "Define AMQP server address and credentials")
+	inQueueName = flag.String("in-queue", "sport-archive-request", "Define AMQP server address and credentials")
+	//outQueueName = flag.String("out-queue", "sport-archive-response", "Define AMQP server address and credentials")
+	allowSave = flag.Bool("allow-save", true, "")
+	logPath   = flag.String("log-path", "./access.log", "")
+	verbose   = flag.Bool("verbose", true, "Print info level logs to stdout")
 
 	zipkinAddr = flag.String("zipkin-addr", "http://127.0.0.1:9411", "Select zipkin host")
 )
@@ -45,8 +50,7 @@ func main() {
 
 	defer logger.Init(serviceName, *verbose, false, logFile).Close()
 
-	port := http.ParsePortFromAddr(*serveAddr)
-	tracer, err := trace.NewTracer(*zipkinAddr, serviceName, port)
+	tracer, err := trace.NewTracer(*zipkinAddr, serviceName, 0)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -59,16 +63,93 @@ func main() {
 	db.AutoMigrate(&domain.Sport{})
 	db.AutoMigrate(&domain.Participant{})
 
-	svc := service.NewSportArchiveService(db, *allowSaveData)
+	svc := service.NewSportArchiveService(db, *allowSave)
 	eps := endpoint.NewSportArchiveService(svc)
-	handler.HandleClientsRequests(
-		*serveAddr,
-		eps,
-		zipkinhttp.NewServerMiddleware(
-			tracer,
-			zipkinhttp.SpanName("request"),
-		),
+
+	server := rpc.NewServer()
+	server.RegisterCodec(json.NewCodec(), "application/json")
+	server.RegisterCodec(json.NewCodec(), "application/json;charset=UTF-8")
+
+	err = server.RegisterService(eps, "SportArchiveService")
+	if err != nil {
+		log.Fatalf("Format of service SportArchiveServiceEndpoint isn't correct. %s", err)
+	}
+
+	conn, err := amqp.Dial(*amqpConnStr)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		*inQueueName, // name
+		false, false, false, false,
+		nil,
 	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.Qos(1, 0, false)
+	failOnError(err, "Failed to set QoS")
+
+	msgs, err := ch.Consume(q.Name, "",
+		false, false, false, false,
+		nil,
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			span := tracer.StartSpan("request")
+
+			httpConn := &HttpConn{}
+
+			logger.Infof("request: %s", string(d.Body))
+
+			_, err := httpConn.Write(d.Body)
+			failOnError(err, "can't create request")
+
+			headers := http.Header{}
+			headers.Set("Content-Type", d.ContentType)
+
+			httpRequest := &http.Request{
+				Method: "POST",
+				Body:   httpConn,
+				Header: headers,
+			}
+
+			httpResponseWtr := &MyResponseWriter{
+				HeaderField: http.Header{},
+				Body:        bytes.NewBufferString(""),
+			}
+			server.ServeHTTP(httpResponseWtr, httpRequest)
+
+			respBytes, _ := ioutil.ReadAll(httpResponseWtr)
+
+			logger.Infof("response: %s", string(respBytes))
+			err = ch.Publish(
+				"",        // exchange
+				d.ReplyTo, // routing key
+				false,     // mandatory
+				false,     // immediate
+				amqp.Publishing{
+					ContentType:   "text/plain",
+					CorrelationId: d.CorrelationId,
+					Body:          respBytes,
+				})
+			failOnError(err, "Failed to publish a message")
+
+			span.Finish()
+
+			d.Ack(false)
+		}
+	}()
+
+	log.Printf(" [*] Awaiting RPC requests")
+	<-forever
 }
 
 func finalizeCloser(c io.Closer) {
@@ -76,4 +157,58 @@ func finalizeCloser(c io.Closer) {
 	if nil != err {
 		logger.Fatalf("Failed to close descriptor: %v", err)
 	}
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		logger.Fatalf("%s: %s", msg, err)
+	}
+}
+
+type MyResponseWriter struct {
+	HeaderField http.Header
+	//Body        []byte
+	Body *bytes.Buffer
+}
+
+func (rw MyResponseWriter) Header() http.Header {
+	//panic("implement me")
+	return rw.HeaderField
+}
+
+func (rw MyResponseWriter) Write(data []byte) (int, error) {
+	//panic("implement me")
+	//rw.BOdy = bytes.NewBuffer(data)
+	return rw.Body.Write(data)
+	//return len(data), nil
+}
+
+func (rw MyResponseWriter) WriteHeader(statusCode int) {
+	//panic("implement me")
+}
+
+func (rw *MyResponseWriter) Read(p []byte) (n int, err error) {
+	//return c.in.Read(p)
+	return rw.Body.Read(p)
+}
+
+type HttpConn struct {
+	//in  io.Reader
+	//out io.Writer
+
+	buf bytes.Buffer
+}
+
+func (c *HttpConn) Read(p []byte) (n int, err error) {
+	//return c.in.Read(p)
+	return c.buf.Read(p)
+}
+
+func (c *HttpConn) Write(d []byte) (n int, err error) {
+	//return c.out.Write(d)
+	return c.buf.Write(d)
+}
+
+func (c *HttpConn) Close() error {
+	return nil
 }
---
+++ cmd/test-amqp-rpc-client/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+const amqpUrl = "amqp://rabbitmq:rabbitmq@localhost:5672/"
+const queueName = "sport-archive-request"
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func randomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func testRPC() (res string, err error) {
+	conn, err := amqp.Dial(amqpUrl)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when usused
+		true,  // exclusive
+		false, // noWait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	corrId := randomString(32)
+
+	fakeRequest := `{"method":"SportArchiveService.GetSport","params":[{"name":"soccer"}],"id":"123"}`
+
+	err = ch.Publish(
+		"",        // exchange
+		queueName, // routing key
+		false,     // mandatory
+		false,     // immediate
+		amqp.Publishing{
+			ContentType:   "application/json",
+			CorrelationId: corrId,
+			ReplyTo:       q.Name,
+			Body:          []byte(fakeRequest),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	for d := range msgs {
+		if corrId == d.CorrelationId {
+			res = string(d.Body)
+			fmt.Println("response:", res)
+			break
+		}
+	}
+
+	return
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	n := bodyFrom(os.Args)
+
+	log.Printf(" [x] Requesting fib(%d)", n)
+	res, err := testRPC()
+	failOnError(err, "Failed to handle RPC request")
+
+	log.Printf(" [.] Got %s", res)
+}
+
+func bodyFrom(args []string) int {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "30"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+	n, err := strconv.Atoi(s)
+	failOnError(err, "Failed to convert arg to integer")
+	return n
+}
